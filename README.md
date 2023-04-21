9ec82f471eac0b9235371ef39876d132c74c53cc
Hao Cheng <50016026+ulhaocheng@users.noreply.github.com>
2023-04-21 13:31:11 +0100

    add bls12-381 C code

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# mpise
-ISEs for Multi-Precision Integer arithmetic
+# ISEs for Multi-Precision Integer arithmetic
+
+## Overview
+
+- We consider the RISC-V baseline ISA as being `rv64gc` meaning that the following standard extensions
+  - M      (multiplication)
+  - A      (atomic)
+  - F      (single-precision floating-point)
+  - D      (double-precision floating-point)
+  - C      (compressed)
+
+  are available by default.
+
+## Implementations 
+
+- We plan to develop 4 types of implementation for each of X25519 and optimal Ate pairing over BLS12-381:
+  - [x] full-radix    pure-software  x25519
+  - [x] full-radix    ISE-assisted   x25519
+  - [x] reduced-radix pure-software  x25519
+  - [x] reduced-radix ISE-assisted   x25519
+  - [ ] full-radix    pure-software  BLS12-381 pairing
+  - [ ] full-radix    ISE-assisted   BLS12-381 pairing
+  - [ ] reduced-radix pure-software  BLS12-381 pairing
+  - [ ] reduced-radix ISE-assisted   BLS12-381 pairing
+
+## Organization 
+
+```
+├── bin                       - scripts (e.g., environment configuration)
+├── doc                       - documentation (e.g., encoding and design)
+├── src                       - source code
+│   ├── hw                    - hardware part
+│   │   ├── fpga                - source code for the FPGA implementation using Vivado
+│   │   │   ├── board             - source for supporting targeted boards (i.e., sakura-x and arty100T)
+│   │   │   ├── script            - scripts for handling the FPGA bitstream on Vivado
+│   │   │   ├── soc               - the Vivado projects based on the Rocket Chip SoC.
+│   │   │   └── software          - build algorithm-specific sofware running on the FPGA.
+│   │   ├── rocketchip          - source code for ISE-enabled Rocket Chip
+│   │   ├── rtl                 - rtl implementation
+│   │   │   ├── rvzbk             - Implementation of RV64Zbk ISE
+│   │   │   ├── bls12-381         - Implementation of RV64 bls12-381 ISE
+│   │   │   └── x25519            - Implementation of RV64 x25519    ISE
+│   │   └── verilator         - source code for emulator for use with Rocket Chip 
+│   │
+│   ├── bls12-381             - bls12-381 implementations
+│   │   ├── full-radix            - radix-2^64 (pure-sw + ise-assisted)
+│   │   └── reduced-radix         - radix-2^55 (pure-sw + ise-assisted)
+│   └── x25519                - x25519 implementations
+│       ├── full-radix            - radix-2^64 (pure-sw + ise-assisted)
+│       └── reduced-radix         - radix-2^51 (pure-sw + ise-assisted)
+├── bitstream                 - pre-built bitstreams for the arty100T board 
+├── hw-toolchain              - scripts to install RISC-V hardware toolchains 
+├── sw-toolchain              - scripts to install RISC-V software toolchains 
+└── work                      - working directory for build
+```
+
+## Usage 
+
+**TBA**
---
+++ src/bls12-381/full-radix/src/vect.h
@@ -0,0 +1,153 @@
+#ifndef __BLS12_381_ASM_VECT_H__
+#define __BLS12_381_ASM_VECT_H__
+
+
+#include <stdint.h>
+#include <stdio.h>
+
+
+typedef uint64_t limb_t;
+
+// full-radix representation: 64-bit-per-limb
+#define LIMB_T_BITS   64
+
+#define TO_LIMB_T(limb64)     limb64
+
+#define NLIMBS(bits)   (bits/LIMB_T_BITS)
+
+typedef limb_t vec384[NLIMBS(384)];
+typedef limb_t vec768[NLIMBS(768)];
+typedef vec384 vec384x[2];      /* 0 is "real" part, 1 is "imaginary" */
+
+typedef unsigned char byte;
+
+/*
+ * Internal Boolean type, Boolean by value, hence safe to cast to or
+ * reinterpret as 'bool'.
+ */
+typedef limb_t bool_t;
+
+
+/*
+ * Assembly subroutines...
+ */
+void add_mod_384(vec384 ret, const vec384 a, const vec384 b, const vec384 p);
+void cneg_mod_384(vec384 ret, const vec384 a, bool_t flag, const vec384 p);
+void lshift_mod_384(vec384 ret, const vec384 a, size_t count, const vec384 p);
+void cneg_mod_384(vec384 ret, const vec384 a, bool_t flag, const vec384 p);
+
+void mul_mont_384(vec384 ret, const vec384 a, const vec384 b,
+                  const vec384 p, limb_t n0);
+void sqr_mont_384(vec384 ret, const vec384 a, const vec384 p, limb_t n0);
+void redc_mont_384(vec384 ret, const vec768 a, const vec384 p, limb_t n0);
+
+void ct_inverse_mod_383(vec768 ret, const vec384 inp, const vec384 mod,
+                                                      const vec384 modx);
+
+void mul_mont_384x(vec384x ret, const vec384x a, const vec384x b,
+                   const vec384 p, limb_t n0);
+void sqr_mont_384x(vec384x ret, const vec384x a, const vec384 p, limb_t n0);
+void mul_382x(vec768 ret[2], const vec384x a, const vec384x b, const vec384 p);
+void sqr_382x(vec768 ret[2], const vec384x a, const vec384 p);
+
+void add_mod_384x(vec384x ret, const vec384x a, const vec384x b,
+                  const vec384 p);
+void sub_mod_384x(vec384x ret, const vec384x a, const vec384x b,
+                  const vec384 p);
+void mul_by_8_mod_384x(vec384x ret, const vec384x a, const vec384 p);
+void mul_by_3_mod_384x(vec384x ret, const vec384x a, const vec384 p);
+void mul_by_1_plus_i_mod_384x(vec384x ret, const vec384x a, const vec384 p);
+void add_mod_384x384(vec768 ret, const vec768 a, const vec768 b,
+                     const vec384 p);
+void sub_mod_384x384(vec768 ret, const vec768 a, const vec768 b,
+                     const vec384 p);
+
+#define restrict __restrict__
+
+# define launder(var) asm volatile("" : "+r"(var))
+
+static inline void vec_cswap(void *restrict a, void *restrict b, size_t num,
+                             bool_t cbit)
+{
+    limb_t ai, *ap = (limb_t *)a;
+    limb_t bi, *bp = (limb_t *)b;
+    limb_t xorm, mask = (limb_t)0 - cbit;
+    size_t i;
+
+    num /= sizeof(limb_t);
+
+    for (i = 0; i < num; i++) {
+        xorm = ((ai = ap[i]) ^ (bi = bp[i])) & mask;
+        ap[i] = ai ^ xorm;
+        bp[i] = bi ^ xorm;
+    }
+}
+
+static inline void vec_select(void *ret, const void *a, const void *b,
+                              size_t num, bool_t sel_a)
+{
+    launder(sel_a);
+    limb_t bi;
+    volatile limb_t *rp = (limb_t *)ret;
+    const limb_t *ap = (const limb_t *)a;
+    const limb_t *bp = (const limb_t *)b;
+    limb_t xorm, mask = (limb_t)0 - sel_a;
+    size_t i;
+
+    num /= sizeof(limb_t);
+
+    for (i = 0; i < num; i++) {
+        xorm = (ap[i] ^ (bi = bp[i])) & mask;
+        rp[i] = bi ^ xorm;
+    }
+}
+
+static inline bool_t is_zero(limb_t l)
+{
+    limb_t ret = (~l & (l - 1)) >> (LIMB_T_BITS - 1);
+    launder(ret);
+    return ret;
+}
+
+static inline bool_t vec_is_zero(const void *a, size_t num)
+{
+    const limb_t *ap = (const limb_t *)a;
+    limb_t acc;
+    size_t i;
+
+    num /= sizeof(limb_t);
+
+    for (acc = 0, i = 0; i < num; i++)
+        acc |= ap[i];
+
+    return is_zero(acc);
+}
+
+static inline void vec_copy(void *restrict ret, const void *a, size_t num)
+{
+    limb_t *rp = (limb_t *)ret;
+    const limb_t *ap = (const limb_t *)a;
+    size_t i;
+
+    num /= sizeof(limb_t);
+
+    for (i = 0; i < num; i++)
+        rp[i] = ap[i];
+}
+
+static inline void vec_zero(void *ret, size_t num)
+{
+    volatile limb_t *rp = (volatile limb_t *)ret;
+    size_t i;
+
+    num /= sizeof(limb_t);
+
+    for (i = 0; i < num; i++)
+        rp[i] = 0;
+
+#if defined(__GNUC__) || defined(__clang__)
+    asm volatile("" : : "r"(ret) : "memory");
+#endif
+}
+
+#endif /* __BLS12_381_ASM_VECT_H__ */
