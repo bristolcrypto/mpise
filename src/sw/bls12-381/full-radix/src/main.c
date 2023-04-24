086a05d1f774555803bf5e48f4bf7174ad7f3e2d
Hao Cheng <50016026+ulhaocheng@users.noreply.github.com>
2023-04-24 18:36:38 +0100

    add timing measurement

--- src/sw/bls12-381/full-radix/src/main.c
+++ src/sw/bls12-381/full-radix/src/main.c
@@ -5,14 +5,24 @@
 #include "ec_ops.h"
 
 
-void mpi_print(const char *c, const uint64_t *a, int len)
-{
-  int i;
+// for measuring CPU cycles
 
-  printf("%s", c);
-  for (i = len-1; i > 0; i--) printf("%016lX", a[i]);
-  printf("%016lX\n", a[0]);
+static uint64_t rdtsc() {
+  uint64_t tsc = 0;
+
+  __asm__ __volatile__( "rdcycle %0" : "=r" (tsc) );
+
+  return tsc;
 }
+
+#define LOAD_CACHE(X, ITER) for (i = 0; i < (ITER); i++) (X)
+
+#define MEASURE_CYCLES(X, ITER)                    \
+  start_cycles = rdtsc();                          \
+  for (i = 0; i < (ITER); i++) (X);                \
+  end_cycles = rdtsc();                            \
+  diff_cycles = (end_cycles-start_cycles)/(ITER)
+
 
 void test_pairing()
 {
@@ -24,8 +34,10 @@ void test_pairing()
 
   // currently use _P = BLS12_381_G1 and _Q = BLS12_381_G2 and k = 2 
   // to conduct a very simple test
-
-  puts("bilinear test: e(P, [k]Q) == e([k]P, Q)");
+  printf("\n=============================================================\n");
+  printf("bilinear test:\n");
+  printf("=============================================================\n");
+  printf("- e(P, [k]Q) == e([k]P, Q) \n");
   // compute [k]Q 
   POINTonE2_double(_Q, &BLS12_381_G2);
   // compute e1 = e(P, [k]Q)
@@ -39,10 +51,12 @@ void test_pairing()
   POINTonE2_to_affine(Q, &BLS12_381_G2);
   optimal_ate_pairing(e2, Q, P, 1);
   // test whether e1 == e2: i.e., whether e(P, [k]Q) == e([k]P, Q)
-  if (memcmp(e1, e2, sizeof(vec384fp12))) puts("\x1b[31m e1 != e2 \x1b[0m");
-  else puts("\x1b[32m e1 == e2 \x1b[0m");
+  if (memcmp(e1, e2, sizeof(vec384fp12))) 
+    printf("\x1b[31m  e1 != e2 \x1b[0m \n");
+  else 
+    printf("\x1b[32m  e1 == e2 \x1b[0m \n");
 
-  puts("bilinear test: (e(P, [k]Q))^2 == e([2k]P, Q)");
+  printf("- (e(P, [k]Q))^2 == e([2k]P, Q) \n");
   // compute [2k]P 
   POINTonE1_double(_P, &BLS12_381_G1);
   POINTonE1_double(_P, _P);
@@ -53,10 +67,12 @@ void test_pairing()
   // compute e1 = e1^2 
   sqr_fp12(e1, e1);
   // test whether e1 == e2: i.e., whether (e(P, [k]Q)])^2 == e([2k]P, Q)
-  if (memcmp(e1, e2, sizeof(vec384fp12))) puts("\x1b[31m e1 != e2 \x1b[0m");
-  else puts("\x1b[32m e1 == e2 \x1b[0m");
+  if (memcmp(e1, e2, sizeof(vec384fp12))) 
+    printf("\x1b[31m  e1 != e2 \x1b[0m \n");
+  else 
+    printf("\x1b[32m  e1 == e2 \x1b[0m \n");
 
-  puts("bilinear test: (e(P, [k]Q))^4 == e([2k]P, [2]Q)");
+  printf("- (e(P, [k]Q))^4 == e([2k]P, [2]Q) \n");
   // compute [2]Q 
   POINTonE2_double(_Q, &BLS12_381_G2);
   // compute e2 = e([2k]P, [2]Q)
@@ -66,13 +82,108 @@ void test_pairing()
   // compute e1 = e1^2 
   sqr_fp12(e1, e1);
   // test whether e1 == e2: i.e., whether (e(P, [k]Q))^4 == e([2k]P, [2]Q)
-  if (memcmp(e1, e2, sizeof(vec384fp12))) puts("\x1b[31m e1 != e2 \x1b[0m");
-  else puts("\x1b[32m e1 == e2 \x1b[0m");
+  if (memcmp(e1, e2, sizeof(vec384fp12))) 
+    printf("\x1b[31m  e1 != e2 \x1b[0m \n");
+  else 
+    printf("\x1b[32m  e1 == e2 \x1b[0m \n");
+
+  printf("=============================================================\n");
+}
+
+void timing()
+{
+  uint64_t start_cycles, end_cycles, diff_cycles;
+  int i;
+
+  printf("timing measurement:\n");
+  printf("=============================================================\n");
+  printf("fp arith:\n");
+
+  vec384 a, b, r;
+  vec768 z;
+
+  printf("- add_mod_384:        ");
+  LOAD_CACHE(add_mod_384(r, a, b, BLS12_381_P), 1000);
+  MEASURE_CYCLES(add_mod_384(r, a, b, BLS12_381_P), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+  printf("- sub_mod_384:        ");
+  LOAD_CACHE(sub_mod_384(r, a, b, BLS12_381_P), 1000);
+  MEASURE_CYCLES(sub_mod_384(r, a, b, BLS12_381_P), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+  printf("- mul_mont_384:       ");
+  LOAD_CACHE(mul_mont_384(r, a, b, BLS12_381_P, p0), 1000);
+  MEASURE_CYCLES(mul_mont_384(r, a, b, BLS12_381_P, p0), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+  printf("- sqr_mont_384:       ");
+  LOAD_CACHE(sqr_mont_384(r, a, BLS12_381_P, p0), 1000);
+  MEASURE_CYCLES(sqr_mont_384(r, a, BLS12_381_P, p0), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+#if 1
+  printf("- mul_384:            ");
+  LOAD_CACHE(mul_384(z, a, b), 1000);
+  MEASURE_CYCLES(mul_384(z, a, b), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+  printf("- sqr_384:            ");
+  LOAD_CACHE(sqr_384_isa(z, a), 1000);
+  MEASURE_CYCLES(sqr_384_isa(z, a), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+  printf("- _redc_mont_384:     ");
+  LOAD_CACHE(_redc_mont_384_isa(r, z, BLS12_381_P, p0), 1000);
+  MEASURE_CYCLES(_redc_mont_384_isa(r, z, BLS12_381_P, p0), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+  printf("- _redc_once_384:     ");
+  LOAD_CACHE(_redc_once_384_isa(r, a, BLS12_381_P), 1000);
+  MEASURE_CYCLES(_redc_once_384_isa(r, a, BLS12_381_P), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+#endif
+
+  printf("=============================================================\n");
+  printf("fp2 arith:\n");
+
+  vec384x c, d, s;
+
+  printf("- mul_mont_384x:      ");
+  LOAD_CACHE(mul_mont_384x(s, c, d, BLS12_381_P, p0), 1000);
+  MEASURE_CYCLES(mul_mont_384x(s, c, d, BLS12_381_P, p0), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+  printf("- sqr_mont_384x:      ");
+  LOAD_CACHE(sqr_mont_384x(s, c, BLS12_381_P, p0), 1000);
+  MEASURE_CYCLES(sqr_mont_384x(s, c, BLS12_381_P, p0), 10000);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+  printf("=============================================================\n");
+  printf("pairing:\n");
+
+  POINTonE2_affine Q[1];
+  POINTonE1_affine P[1];
+  POINTonE2 _Q[1];
+  POINTonE1 _P[1];
+  vec384fp12 e1;
+
+  POINTonE2_double(_Q, &BLS12_381_G2);
+  POINTonE1_to_affine(P, &BLS12_381_G1);
+  POINTonE2_to_affine(Q, _Q);
+
+  printf("- optimal_ate_pairing:");
+  LOAD_CACHE(optimal_ate_pairing(e1, Q, P, 1), 1);
+  MEASURE_CYCLES(optimal_ate_pairing(e1, Q, P, 1), 10);
+  printf("  #cycle = %lld\n", diff_cycles);
+
+  printf("=============================================================\n");
 }
 
 int main()
 {
   test_pairing();
+  timing();
 
   return 0;
 }
