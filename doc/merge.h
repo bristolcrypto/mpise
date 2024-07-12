fa5ddcea072ab0f507c442f2470509af1d7bb9f7
Dan Page <dan@phoo.org>
2024-07-12 11:04:49 +0100

    notes re. merge of full- and reduced-radix

---
+++ doc/merge.h
@@ -0,0 +1,14 @@
+#ifndef __MERGE_H
+#define __MERGE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define SIZEOF(x) (     sizeof(x) )
+#define BITSOF(x) ( 8 * sizeof(x) )
+
+typedef uint8_t limb_t;
+typedef uint16_t dlimb_t;
+
+#endif
