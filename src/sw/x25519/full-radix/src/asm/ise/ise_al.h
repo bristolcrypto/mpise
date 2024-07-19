1506f7fc456f32c1f2e60568c550e07418529028
Dan Page <dan@phoo.org>
2024-07-19 12:22:40 +0100

    first, non-working attempt to make software work with merged ISE

---
+++ src/sw/x25519/full-radix/src/asm/ise/ise_al.h
@@ -0,0 +1,56 @@
+// Copyright (C) 2021 SCARV project <info@scarv.org>
+//
+// Use of this source code is restricted per the MIT license, a copy of which 
+// can be found at https://opensource.org/licenses/MIT (or should be included 
+// as LICENSE.txt within the associated archive or repository).
+
+// ============================================================================
+
+#ifndef _ISE_AL_H
+#define _ISE_AL_H
+
+#include "ise.h"
+
+.macro maddlu Z, A, B, S
+#if   defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 0 )
+// !destructive
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+// !destructive && !stateless
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+// !destructive &&  stateless
+  macclu \Z, \A, \B, \S, 15
+#endif
+#elif defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 1 )
+//  destructive
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+//  destructive && !stateless
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+//  destructive &&  stateless
+  macclu \Z, \A, \B,     15
+#endif
+#endif
+.endm
+
+.macro maddhu Z, A, B, S
+#if   defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 0 )
+// !destructive
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+// !destructive && !stateless
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+// !destructive &&  stateless
+  macchu \Z, \A, \B, \S, 15
+#endif
+#elif defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 1 )
+//  destructive
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+//  destructive && !stateless
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+//  destructive &&  stateless
+  macchu \Z, \A, \B,     15
+#endif
+#endif
+.endm
+
+// ============================================================================                              
+
+#endif 
