1d06de6c0b45c9a5954371395fb720af6cfe3cce
Dan Page <dan@phoo.org>
2025-03-04 12:38:46 +0000

    step toward rv32 support

--- src/sw/intmul/scan.sh
+++ src/sw/intmul/scan.sh
@@ -4,47 +4,51 @@
 # bash ./scan.sh collect => build executables into ./scan
 # bash ./scan.sh execute => execute executables, and so generate benchmarking output into ./scan
 
+export ARCH="rv64"
+export RADIXES="64 63 62 61 60 59 58 57 56 55 54 53 52 51 50"
+export PLATFORM="spike"
+
 # =============================================================================
 
-RADIXES="64 63 62 61 60 59 58 57 56 55 54 53 52 51 50"
-PLATFORM="spike"
-
 function name() {
-  echo "./scan/intmul-rv64_${1}_radix=${2}.${3}"
+  echo "./scan/intmul-${ARCH}_${TYPE}_radix=${1}.${2}"
 }
 
 function header() {
-  echo "===="                                       | tee --append $(name ${1} ${2} log_${3})
-  echo "REPO_HOME         := ${REPO_HOME}"          | tee --append $(name ${1} ${2} log_${3})
-  echo "RISCV             := ${RISCV}"              | tee --append $(name ${1} ${2} log_${3})
-  echo "MPISE_DESTRUCTIVE := ${MPISE_DESTRUCTIVE}"  | tee --append $(name ${1} ${2} log_${3})
-  echo "MPISE_STATELESS   := ${MPISE_STATELESS}"    | tee --append $(name ${1} ${2} log_${3})
+  echo "===="                                       | tee --append $(name ${1} log)
+  echo "REPO_HOME         := ${REPO_HOME}"          | tee --append $(name ${1} log)
+  echo "RISCV             := ${RISCV}"              | tee --append $(name ${1} log)
+  echo "MPISE_DESTRUCTIVE := ${MPISE_DESTRUCTIVE}"  | tee --append $(name ${1} log)
+  echo "MPISE_STATELESS   := ${MPISE_STATELESS}"    | tee --append $(name ${1} log)
   echo "-----------"
-  echo "TYPE              := ${1}"                  | tee --append $(name ${1} ${2} log_${3})
-  echo "RADIX             := ${2}"                  | tee --append $(name ${1} ${2} log_${3})
-  echo "PLATFORM          := ${3}"                  | tee --append $(name ${1} ${2} log_${3})
-  echo "<<<<"                                       | tee --append $(name ${1} ${2} log_${3})
+  echo "ARCH              := ${ARCH}"               | tee --append $(name ${1} log)
+  echo "TYPE              := ${TYPE}"               | tee --append $(name ${1} log)
+  echo "RADIX             := ${1}"                  | tee --append $(name ${1} log)
+  echo "PLATFORM          := ${PLATFORM}"           | tee --append $(name ${1} log)
+  echo "<<<<"                                       | tee --append $(name ${1} log)
 }
 
 function footer() {
-  echo ">>>>"                                       | tee --append $(name ${1} ${2} log_${3})
+  echo ">>>>"                                       | tee --append $(name ${1} log)
 }
 
 # -----------------------------------------------------------------------------
 
 case ${1} in
   clean)
-    rm --force ./scan/*
+    rm --force --recursive ${REPO_HOME}/build/intmul ./scan/*
     ;;
 
   collect)
     for RADIX in ${RADIXES} ; do
       # build then relocate ISA-based benchmark
-      make --directory="${REPO_HOME}" ALG="intmul" ARCH="rv64" TYPE="isa" RADIX="${RADIX}" PLATFORM="${PLATFORM}" sw-clean sw-build
-      mv ${REPO_HOME}/build/intmul/intmul-rv64_isa.elf $(name isa ${RADIX} elf)
+      export TYPE="isa"
+      make --directory="${REPO_HOME}" ALG="intmul" ARCH="${ARCH}" TYPE="${TYPE}" RADIX="${RADIX}" PLATFORM="${PLATFORM}" sw-clean sw-build
+      mv ${REPO_HOME}/build/intmul/intmul-${ARCH}_isa.elf $(name ${RADIX} elf)
       # build then relocate ISE-based benchmark
-      make --directory="${REPO_HOME}" ALG="intmul" ARCH="rv64" TYPE="ise" RADIX="${RADIX}" PLATFORM="${PLATFORM}" sw-clean sw-build
-      mv ${REPO_HOME}/build/intmul/intmul-rv64_ise.elf $(name ise ${RADIX} elf)
+      export TYPE="ise"
+      make --directory="${REPO_HOME}" ALG="intmul" ARCH="${ARCH}" TYPE="${TYPE}" RADIX="${RADIX}" PLATFORM="${PLATFORM}" sw-clean sw-build
+      mv ${REPO_HOME}/build/intmul/intmul-${ARCH}_ise.elf $(name ${RADIX} elf)
     done
     ;;
 
@@ -52,22 +56,26 @@ case ${1} in
     for RADIX in ${RADIXES} ; do
       if   [ "${PLATFORM}" == "spike" ] ; then
         # execute ISA-based benchmark
-        header isa ${RADIX} ${PLATFORM}
-        ${RISCV}/bin/spike --isa='rv64imafdc_zicsr_zifencei_zicntr' ${RISCV}/riscv64-unknown-elf/bin/pk $(name isa ${RADIX} elf) | tee --append $(name isa ${RADIX} log_${PLATFORM})
-        footer isa ${RADIX} ${PLATFORM}
+        export TYPE="isa"
+        header ${RADIX}
+        ${RISCV}/bin/spike --isa='rv64imafdc_zicsr_zifencei_zicntr' ${RISCV}/riscv64-unknown-elf/bin/pk $(name ${RADIX} elf) | tee --append $(name ${RADIX} log)
+        footer ${RADIX}
         # execute ISE-based benchmark
+        export TYPE="ise"
         header ise ${RADIX} ${PLATFORM}
-        ${RISCV}/bin/spike --isa='rv64imafdc_zicsr_zifencei_zicntr' ${RISCV}/riscv64-unknown-elf/bin/pk $(name ise ${RADIX} elf) | tee --append $(name ise ${RADIX} log_${PLATFORM})
+        ${RISCV}/bin/spike --isa='rv64imafdc_zicsr_zifencei_zicntr' ${RISCV}/riscv64-unknown-elf/bin/pk $(name ${RADIX} elf) | tee --append $(name ${RADIX} log)
         footer ise ${RADIX} ${PLATFORM}
 
       elif [ "${PLATFORM}" == "cva6"  ] ; then
         # execute ISA-based benchmark
+        export TYPE="isa"
         header isa ${RADIX} ${PLATFORM}
-                                                                                                        $(name isa ${RADIX} elf) | tee --append $(name isa ${RADIX} log_${PLATFORM})
+                                                                                                        $(name ${RADIX} elf) | tee --append $(name ${RADIX} log)
         footer isa ${RADIX} ${PLATFORM}
         # execute ISE-based benchmark
+        export TYPE="ise"
         header ise ${RADIX} ${PLATFORM}
-                                                                                                        $(name ise ${RADIX} elf) | tee --append $(name ise ${RADIX} log_${PLATFORM})
+                                                                                                        $(name ${RADIX} elf) | tee --append $(name ${RADIX} log)
         footer ise ${RADIX} ${PLATFORM}
       fi
     done
--- src/sw/share/arch/rv32/ise.h
+++ src/sw/share/arch/rv32/ise.h
@@ -5,5 +5,78 @@
 // as LICENSE.txt within the associated archive or repository).
 
 // ============================================================================
+// 1. support for CSR access
+
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+//                 !stateless
+.macro maccwri imm
+// issue    CSR update
+csrrwi x0, 0x801, \imm
+// complete CSR update => stall to cater for CVA6 pipeline
+.rept 10
+nop
+.endr
+.endm
+#endif
+
+// ----------------------------------------------------------------------------
+// 2. support for carry propagation
+
+.macro sraiadd  rd, rs1, rs2, imm
+.insn r  CUSTOM_0, 1, ( \imm & 0x7F ) << 0, \rd, \rs1, \rs2
+.endm 
+
+#if   defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 0 )
+// !destructive
+.macro cacc rd, rs1, rs2, rs3             
+.insn r4 CUSTOM_0, 0, 0, \rd, \rs1, \rs2, \rs3 
+.endm
+#elif defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 1 )
+//  destructive
+.macro cacc rd, rs1, rs2
+.insn r  CUSTOM_0, 0, 0, \rd, \rs1, \rs2
+.endm
+#endif
+
+// ----------------------------------------------------------------------------
+// 3. support for multiply-add
+
+#if   defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 0 )
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+// !destructive && !stateless
+.macro macclu rd, rs1, rs2, rs3
+.insn r4 CUSTOM_1, 0, 0, \rd, \rs1, \rs2, \rs3
+.endm
+.macro macchu rd, rs1, rs2, rs3
+.insn r4 CUSTOM_1, 1, 0, \rd, \rs1, \rs2, \rs3
+.endm
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+// !destructive &&  stateless
+.macro macclu rd, rs1, rs2, rs3, imm
+.insn r4 CUSTOM_1, 0 | ( ( \imm & 0x3 ) << 1 ), ( \imm & 0xC ) >> 2, \rd, \rs1, \rs2, \rs3
+.endm
+.macro macchu rd, rs1, rs2, rs3, imm
+.insn r4 CUSTOM_1, 1 | ( ( \imm & 0x3 ) << 1 ), ( \imm & 0xC ) >> 2, \rd, \rs1, \rs2, \rs3
+.endm
+#endif
+#elif defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 1 )
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+//  destructive && !stateless
+.macro macclu rd, rs1, rs2
+.insn r  CUSTOM_1, 0, 0, \rd, \rs1, \rs2
+.endm
+.macro macchu rd, rs1, rs2
+.insn r  CUSTOM_1, 1, 0, \rd, \rs1, \rs2
+.endm
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+//  destructive &&  stateless
+.macro macclu rd, rs1, rs2, imm
+.insn r  CUSTOM_1, 0 | ( ( \imm & 0x3 ) << 1 ), ( \imm & 0xC ) >> 2, \rd, \rs1, \rs2
+.endm
+.macro macchu rd, rs1, rs2, imm
+.insn r  CUSTOM_1, 1 | ( ( \imm & 0x3 ) << 1 ), ( \imm & 0xC ) >> 2, \rd, \rs1, \rs2
+.endm
+#endif
+#endif
 
 // ============================================================================
