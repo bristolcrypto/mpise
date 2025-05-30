94085c5d76413f32a0c7763af982328d1afda979
Dan Page <dan@phoo.org>
2025-05-30 13:16:56 +0100

    update script to fine-tune radix based on ISA- or ISE-based cases

--- src/sw/intmul/scan.sh
+++ src/sw/intmul/scan.sh
@@ -12,18 +12,25 @@ if [ -z ${ARCH}     ] ; then
   export ARCH="rv64"
 fi
 
-if [ -z ${RADIXES}  ] ; then
+update_radixes() {
   if   [ "${ARCH}" = "rv32" ] ; then
-    export RADIXES="32 31 30 29 28 27 26 25"
+    if   [ "${TYPE}" = "isa" ] ; then
+      export RADIXES="32 30 29 28 27 26 25 24 23 22 21 20"       # full-radix = XLEN = 32, plus reduced-radix down from XLEN-2 = 32-2 = 30
+    elif [ "${TYPE}" = "ise" ] ; then
+      export RADIXES="32    29 28 27 26 25 24 23 22 21 20"       # full-radix = XLEN = 32, plus reduced-radix down from XLEN-3 = 32-3 = 29
+    fi
   elif [ "${ARCH}" = "rv64" ] ; then
-    export RADIXES="64 63 62 61 60 59 58 57 56 55 54 53 52 51 50"
+    if   [ "${TYPE}" = "isa" ] ; then
+      export RADIXES="64 62 61 60 59 58 57 56 55 54 53 52 51 50" # full-radix = XLEN = 64, plus reduced-radix down from XLEN-2 = 64-2 = 62
+    elif [ "${TYPE}" = "ise" ] ; then
+      export RADIXES="64    61 60 59 58 57 56 55 54 53 52 51 50" # full-radix = XLEN = 64, plus reduced-radix down from XLEN-3 = 64-3 = 61
+    fi
   fi
-fi
+}
 
 echo "PLATFORM = ${PLATFORM}"
 echo "RISCV    = ${RISCV}"
 echo "ARCH     = ${ARCH}"
-echo "RADIXES  = ${RADIXES}"
 
 # =============================================================================
 
@@ -36,16 +43,22 @@ case ${1} in
     ;;
 
   collect)
+    # build then relocate ISA-based benchmarks
+    export TYPE="isa"
+    update_radixes
+    echo "-- doing TYPE=${TYPE}, RADIXES=${RADIXES}"
     for RADIX in ${RADIXES} ; do
-      # build then relocate ISA-based benchmarks
-      export TYPE="isa"
       export RADIX
       export VERSION="simple"
       make --quiet --directory="${REPO_HOME}/src/sw/intmul" --file="${REPO_HOME}/src/sw/intmul/scan.mk" ${REPO_HOME}/src/sw/intmul/scan/intmul-${ARCH}_${TYPE}_${RADIX}_${VERSION}.elf
       export VERSION="hybrid"
       make --quiet --directory="${REPO_HOME}/src/sw/intmul" --file="${REPO_HOME}/src/sw/intmul/scan.mk" ${REPO_HOME}/src/sw/intmul/scan/intmul-${ARCH}_${TYPE}_${RADIX}_${VERSION}.elf
-      # build then relocate ISE-based benchmarks
-      export TYPE="ise"
+    done
+    # build then relocate ISE-based benchmarks
+    export TYPE="ise"
+    update_radixes
+    echo "-- doing TYPE=${TYPE}, RADIXES=${RADIXES}"
+    for RADIX in ${RADIXES} ; do
       export RADIX
       export VERSION="simple"
       make --quiet --directory="${REPO_HOME}/src/sw/intmul" --file="${REPO_HOME}/src/sw/intmul/scan.mk" ${REPO_HOME}/src/sw/intmul/scan/intmul-${ARCH}_${TYPE}_${RADIX}_${VERSION}.elf
@@ -55,16 +68,22 @@ case ${1} in
     ;;
 
   execute)
+    # execute ISA-based benchmarks
+    export TYPE="isa"
+    update_radixes
+    echo "-- doing TYPE=${TYPE}, RADIXES=${RADIXES}"
     for RADIX in ${RADIXES} ; do
-      # execute ISA-based benchmarks
-      export TYPE="isa"
       export RADIX
       export VERSION="simple"
       make --quiet --directory="${REPO_HOME}/src/sw/intmul" --file="${REPO_HOME}/src/sw/intmul/scan.mk" ${REPO_HOME}/src/sw/intmul/scan/intmul-${ARCH}_${TYPE}_${RADIX}_${VERSION}.log
       export VERSION="hybrid"
       make --quiet --directory="${REPO_HOME}/src/sw/intmul" --file="${REPO_HOME}/src/sw/intmul/scan.mk" ${REPO_HOME}/src/sw/intmul/scan/intmul-${ARCH}_${TYPE}_${RADIX}_${VERSION}.log
-      # execute ISE-based benchmarks
-      export TYPE="ise"
+    done
+    # execute ISE-based benchmarks
+    export TYPE="ise"
+    update_radixes
+    echo "-- doing TYPE=${TYPE}, RADIXES=${RADIXES}"
+    for RADIX in ${RADIXES} ; do
       export RADIX
       export VERSION="simple"
       make --quiet --directory="${REPO_HOME}/src/sw/intmul" --file="${REPO_HOME}/src/sw/intmul/scan.mk" ${REPO_HOME}/src/sw/intmul/scan/intmul-${ARCH}_${TYPE}_${RADIX}_${VERSION}.log
