8342bded5bb96390ede887ba2988ba9434fcd15f
Johann Großschädl <johann.groszschaedl@uni.lu>
2025-06-16 01:32:52 +0200

    Unification of test functions for RV32 and RV64

--- src/sw/x25519/full/main.c
+++ src/sw/x25519/full/main.c
@@ -3,266 +3,302 @@
 #include "gfparith.h"
 #include "moncurve.h"
 
-#define NLMB64 4
 
 // macros for measuring CPU cycles
 
-#define LOAD_CACHE(X, ITER) for (i = 0; i < (ITER); i++) (X)
+#define LOAD_CACHE(X, iter) do {                  \
+  for (i = 0; i < (iter); i++) (X);               \
+} while (0);
 
-#define MEASURE_CYCLES(X, ITER)                    \
-  start_cycles = rdtsc();                          \
-  for (i = 0; i < (ITER); i++) (X);                \
-  end_cycles = rdtsc();                            \
-  diff_cycles = (end_cycles-start_cycles)/(ITER)
+#define MEASURE_CYCLES(X, iter) do {              \
+  start_cycles = rdtsc();                         \
+  for (i = 0; i < (iter); i++) (X);               \
+  end_cycles = rdtsc();                           \
+  diff_cycles = (end_cycles-start_cycles)/(iter); \
+} while (0);
 
 
-void test_gfp_arith()
+void test_gfp_arith(int iter)
 {
   uint64_t start_cycles, end_cycles, diff_cycles;
+  Word op1f[NWORDS], op2f[NWORDS], resf[NWORDS];  // full-radix
+  unsigned char resh[2*WBYTES*NWORDS+3];
   int i;
-
-  // a := 0x0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF;
-  uint64_t a[NLMB64] = { 0x0123456789ABCDEFULL, 0x0123456789ABCDEFULL,
-                         0x0123456789ABCDEFULL, 0x0123456789ABCDEFULL, };
-  // b := 0x09ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF01234567;
-  uint64_t b[NLMB64] = { 0x89ABCDEF01234567ULL, 0x89ABCDEF01234567ULL,
-                         0x89ABCDEF01234567ULL, 0x09ABCDEF01234567ULL, };
-  uint64_t r[NLMB64];
-
+  
+  static const unsigned char op1h[] =  // 1st operand for testing
+    "0x0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF";
+  static const unsigned char op2h[] =  // 2nd operand for testing
+    "0x89ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF01234567";
+  
+  hex_to_int(op1f, op1h, NWORDS);
+  hex_to_int(op2f, op2h, NWORDS);
+  
   printf("=============================================================\n");
   printf("timing measurement:\n");
   printf("=============================================================\n");
   printf("gfp arith:\n");
-
+  
   printf("- gfp mul:     ");
-
-  LOAD_CACHE(gfp_mul(r, a, b), 100);
-  MEASURE_CYCLES(gfp_mul(r, a, b), 1000);
-  printf("         #cycle = %lld\n", diff_cycles);
-
+  LOAD_CACHE(gfp_mul(resf, op1f, op2f), 10);
+  MEASURE_CYCLES(gfp_mul(resf, op1f, op2f), iter);
+  printf("         #cycles = %lld\n", diff_cycles);
+  
 #if DEBUG
-  // r := 0x359FEE6106B5DA2E7C347321EADF6B4942C8F7E2CF08FC64095D7CA3B3328D77;
-  mpi_print("  r  = 0x", r, NLMB64);
-  memset(r, 0, sizeof(uint64_t)*NLMB64);
+  int_to_hex(resh, resf, NWORDS);
+  printf("  r  = %s\n", resh);
+  static const unsigned char mulh[] =  // expected result gfp_mul
+    "0x4B3E15103E7622EB91D299D1229FB40658671E9206C945211EFBA352EAF2D634";
+  if (strcmp(mulh, resh) != 0) printf("  result r is wrong!!!\n");
+  memset(resf, 0, WBYTES*NWORDS);
 #endif
-
-  // ---------------------------------------------------------------------------
-
-
+  
+  // --------------------------------------------------------------------------
+  
   printf("- gfp sqr:     ");
-
-  LOAD_CACHE(gfp_sqr(r, a), 100);
-  MEASURE_CYCLES(gfp_sqr(r, a), 1000);
-  printf("         #cycle = %lld\n", diff_cycles);
-
+  LOAD_CACHE(gfp_sqr(resf, op1f), 10);
+  MEASURE_CYCLES(gfp_sqr(resf, op1f), iter);
+  printf("         #cycles = %lld\n", diff_cycles);
+  
 #if DEBUG
-  // r := 0x72CC9B9B881D163356F62CB64EB19AF43B1FBDD115461FB51F494EEBDBDAA465;
-  mpi_print("  r  = 0x", r, NLMB64);
-  memset(r, 0, sizeof(uint64_t)*NLMB64);
+  int_to_hex(resh, resf, NWORDS);
+  printf("  r  = %s\n", resh);
+  static const unsigned char sqrh[] =  // expected result gfp_sqr
+    "0x72CC9B9B881D163356F62CB64EB19AF43B1FBDD115461FB51F494EEBDBDAA465";
+  if (strcmp(sqrh, resh) != 0) printf("  result r is wrong!!!\n");
+  memset(resf, 0, WBYTES*NWORDS);
 #endif
-
-  // ---------------------------------------------------------------------------
-
+  
+  // --------------------------------------------------------------------------
+  
   printf("- gfp add:     ");
-
-  LOAD_CACHE(gfp_add(r, a, b), 100);
-  MEASURE_CYCLES(gfp_add(r, a, b), 1000);
-  printf("         #cycle = %lld\n", diff_cycles);
-
+  LOAD_CACHE(gfp_add(resf, op1f, op2f), 10);
+  MEASURE_CYCLES(gfp_add(resf, op1f, op2f), iter);
+  printf("         #cycles = %lld\n", diff_cycles);
+  
 #if DEBUG
-  //r  = 0x0acf13568acf13568acf13568acf13568acf13568acf13568acf13568acf1356
-  mpi_print("  r  = 0x", r, NLMB64);
-  memset(r, 0, sizeof(uint64_t)*NLMB64);
+  int_to_hex(resh, resf, NWORDS);
+  printf("  r  = %s\n", resh);
+  static const unsigned char addh[] =  // expected result gfp_add
+    "0x0ACF13568ACF13568ACF13568ACF13568ACF13568ACF13568ACF13568ACF1369";
+  if (strcmp(addh, resh) != 0) printf("  result r is wrong!!!\n");
+  memset(resf, 0, WBYTES*NWORDS);
 #endif
-
-  // ---------------------------------------------------------------------------
-
+  
+  // --------------------------------------------------------------------------
+  
   printf("- gfp sub:     ");
-
-  LOAD_CACHE(gfp_sub(r, a, b), 100);
-  MEASURE_CYCLES(gfp_sub(r, a, b), 1000);
-  printf("         #cycle = %lld\n", diff_cycles);
-
+  LOAD_CACHE(gfp_sub(resf, op1f, op2f), 10);
+  MEASURE_CYCLES(gfp_sub(resf, op1f, op2f), iter);
+  printf("         #cycles = %lld\n", diff_cycles);
+  
 #if DEBUG
-  //r  = 0x7777777888888887777777788888888777777778888888877777777888888875
-  mpi_print("  r  = 0x", r, NLMB64);
-  memset(r, 0, sizeof(uint64_t)*NLMB64);
+  int_to_hex(resh, resf, NWORDS);
+  printf("  r  = %s\n", resh);
+  static const unsigned char subh[] =  // expected result gfp_sub
+    "0x7777777888888887777777788888888777777778888888877777777888888862";
+  if (strcmp(subh, resh) != 0) printf("  result r is wrong!!!\n");
+  memset(resf, 0, WBYTES*NWORDS);
 #endif
-
-  // ---------------------------------------------------------------------------
-
+  
+  // --------------------------------------------------------------------------
+  
   printf("- gfp mul32:     ");
-
-  LOAD_CACHE(gfp_mul32(r, a, (CONSTA+2)/4), 100);
-  MEASURE_CYCLES(gfp_mul32(r, a, (CONSTA+2)/4), 1000);
-  printf("       #cycle = %lld\n", diff_cycles);
-
+  LOAD_CACHE(gfp_mul32(resf, op1f, (CONSTA + 2)/4), 10);
+  MEASURE_CYCLES(gfp_mul32(resf, op1f, (CONSTA + 2)/4), iter);
+  printf("       #cycles = %lld\n", diff_cycles);
+  
 #if DEBUG
-  //r  = 0x3cdf012345658ebabcdf012345658ebabcdf012345658ebabcdf01234565dcd9
-  mpi_print("  r  = 0x", r, NLMB64);
-  memset(r, 0, sizeof(uint64_t)*NLMB64);
+  int_to_hex(resh, resf, NWORDS);
+  printf("  r  = %s\n", resh);
+  static const unsigned char m32h[] =  // expected result gfp_mul32
+    "0x3CDF012345658EBABCDF012345658EBABCDF012345658EBABCDF01234565DCD9";
+  if (strcmp(m32h, resh) != 0) printf("  result r is wrong!!!\n");
+  memset(resf, 0, WBYTES*NWORDS);
 #endif
 }
 
-void test_curve_arith()
+
+void test_curve_arith(int iter)
 {
   uint64_t start_cycles, end_cycles, diff_cycles;
-  int i;
-
-  uint64_t xp[NLMB64] = { 0x89ABCDEF01234A14, 0x89ABCDEF01234567, \
-    0x89ABCDEF01234567, 0x09ABCDEF01234567 };
-  uint64_t zp[NLMB64] = { 0x0123456789ABD289, 0x0123456789ABCDEF, \
-    0x0123456789ABCDEF, 0x0123456789ABCDEF };  
-  uint64_t xq[NLMB64] = { 0xa3030242d807aa98, 0x45706fbe12835b01, \
-    0x4ee4b28c243185be, 0x55ffb4e2550c7dc3 };
-  uint64_t zq[NLMB64] = { 0xf27b896f72be5d74, 0x3b1696b180deb1fe, \
-    0x25c712359bdc06a7, 0x4f692694c19bf174 };
-  uint64_t xd[NLMB64] = { 0x9ef14ad2e49b69c1, 0x384f25e3efbe4786, \
-    0x8b8cd5b50fc19dc6, 0x77ac9c65240ca1cc };
+  Word rf[NWORDS], kf[NWORDS], xf[NWORDS];  // full-radix
+  unsigned char resh[2*WBYTES*NWORDS+3];
   ProPoint p, q;
-  uint64_t r[NLMB64];
-
+  int i;
+  
+  static const unsigned char xpih[] =  // X-coordinate of 1st point
+    "0x09ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF01234A14";
+  static const unsigned char zpih[] =  // Z-coordinate of 1st point
+    "0x0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABD289";
+  static const unsigned char xqih[] =  // X-coordinate of 2nd point
+    "0x55FFB4E2550C7DC34EE4B28C243185BE45706FBE12835B01A3030242D807AA98";
+  static const unsigned char zqih[] =  // Z-coordinate of 2nd point
+    "0x4F692694C19BF17425C712359BDC06A73B1696B180DEB1FEF27B896F72BE5D74";
+  static const unsigned char xdih[] =  // x-coordinate of basepoint
+    "0x77AC9C65240CA1CC8B8CD5B50FC19DC6384F25E3EFBE47869EF14AD2E49B69C1";
+  
   printf("-------------------------------------------------------------\n");
   printf("montgomery curve arithmetic:\n");
-
-  memcpy(p.x, xp, sizeof(uint64_t)*NLMB64);
-  memcpy(p.z, zp, sizeof(uint64_t)*NLMB64);
-  memcpy(q.x, xq, sizeof(uint64_t)*NLMB64);
-  memcpy(q.z, zq, sizeof(uint64_t)*NLMB64);
-
+  
+  hex_to_int(p.x, xpih, NWORDS);
+  hex_to_int(p.z, zpih, NWORDS);
+  hex_to_int(q.x, xqih, NWORDS);
+  hex_to_int(q.z, zqih, NWORDS);
+  hex_to_int(xf,  xdih, NWORDS);
+  
   printf("- mon ladder step:     ");
-
-  LOAD_CACHE(mon_ladder_step(&p, &q, xd), 10);
-  MEASURE_CYCLES(mon_ladder_step(&p, &q, xd), 100);
-  printf(" #cycle = %lld\n", diff_cycles);
-
+  LOAD_CACHE(mon_ladder_step(&p, &q, xf), 10);
+  MEASURE_CYCLES(mon_ladder_step(&p, &q, xf), 10*iter);
+  printf(" #cycles = %lld\n", diff_cycles);
+  
 #if DEBUG
-  memcpy(p.x, xp, sizeof(uint64_t)*NLMB64);
-  memcpy(p.z, zp, sizeof(uint64_t)*NLMB64);
-  memcpy(q.x, xq, sizeof(uint64_t)*NLMB64);
-  memcpy(q.z, zq, sizeof(uint64_t)*NLMB64);
-  // x3 := 0x4F46FD1FD643F60F58FBC3DCB6691EDBB8BC4C0E8619879A1E7CCD145F86813B
-  // z3 := 0x32C0C112720E8B7C9945DA6D5730D5D9CD118B8C76A5A8E7AB247B06822B993C
-  // x2 := 0x470F51948095E1CFB7C4EACC0FE04898287A84039F2AAF6099301D3BB17EC40A
-  // z2 := 0x6BC1BD72824403011C63885844A207D88F7470C8B423A1FA6DC7CA52785B2725
-  mon_ladder_step(&p, &q, xd); 
-  mpi_print("  x3 = 0x", q.x, 4);
-  mpi_print("  z3 = 0x", q.z, 4);
-  mpi_print("  x2 = 0x", p.x, 4);
-  mpi_print("  z2 = 0x", p.z, 4);
+  hex_to_int(p.x, xpih, NWORDS);
+  hex_to_int(p.z, zpih, NWORDS);
+  hex_to_int(q.x, xqih, NWORDS);
+  hex_to_int(q.z, zqih, NWORDS);
+  mon_ladder_step(&p, &q, xf);
+  // check X-coodinate of P
+  int_to_hex(resh, p.x, NWORDS);
+  printf("  Xp = %s\n", resh);
+  static const unsigned char xpoh[] =  // expected result for Xp
+    "0x470F51948095E1CFB7C4EACC0FE04898287A84039F2AAF6099301D3BB17EC40A";
+  if (strcmp(xpoh, resh) != 0) printf("  result Xp is wrong!!!\n");
+  // check Z-coodinate of P
+  int_to_hex(resh, p.z, NWORDS);
+  printf("  Zp = %s\n", resh);
+  static const unsigned char zpoh[] =  // expected result for Zp
+    "0x6BC1BD72824403011C63885844A207D88F7470C8B423A1FA6DC7CA52785B2725";
+  if (strcmp(zpoh, resh) != 0) printf("  result Zp is wrong!!!\n");
+  // check X-coodinate of Q
+  int_to_hex(resh, q.x, NWORDS);
+  printf("  Xq = %s\n", resh);
+  static const unsigned char xqoh[] =  // expected result for Xq
+    "0x4F46FD1FD643F60F58FBC3DCB6691EDBB8BC4C0E8619879A1E7CCD145F86813B";
+  if (strcmp(xqoh, resh) != 0) printf("  result Xq is wrong!!!\n");
+  // check Z-coodinate of Q
+  int_to_hex(resh, q.z, NWORDS);
+  printf("  Zq = %s\n", resh);
+  static const unsigned char zqoh[] =  // expected result for Zq
+    "0x32C0C112720E8B7C9945DA6D5730D5D9CD118B8C76A5A8E7AB247B06822B993C";
+  if (strcmp(zqoh, resh) != 0) printf("  result Zq is wrong!!!\n");
 #endif 
-
-  // ---------------------------------------------------------------------------
-
+  
+  // --------------------------------------------------------------------------
+  
+  static const unsigned char kh[] =  // scalar k for testing
+    "0xC49A44BA44226A50185AFCC10A4C1462DD5E46824B15163B9D7C52F06BE346A5";
+  static const unsigned char xh[] =  // x-coordinate for testing
+    "0x4C1CABD0A603A9103B35B326EC2466727C5FB124A4C19435DB3030586768DBE6";
+  
+  hex_to_int(kf, kh, NWORDS);
+  hex_to_int(xf, xh, NWORDS);
+  
   printf("- mon varbase mul:     ");
-
-  uint64_t k[NLMB64] = { 0x9D7C52F06BE346A5ULL, 0xDD5E46824B15163BULL, \
-    0x185AFCC10A4C1462ULL, 0xC49A44BA44226A50ULL };
-  uint64_t x[NLMB64] = { 0xDB3030586768DBE6ULL, 0x7C5FB124A4C19435ULL, \
-    0x3B35B326EC246672ULL, 0x4C1CABD0A603A910ULL };
-
-  LOAD_CACHE(mon_mul_varbase(r, k, x), 10);
-  MEASURE_CYCLES(mon_mul_varbase(r, k, x), 50);
-  printf(" #cycle = %lld\n", diff_cycles);
-
+  LOAD_CACHE(mon_mul_varbase(rf, kf, xf), 10);
+  MEASURE_CYCLES(mon_mul_varbase(rf, kf, xf), iter);
+  printf(" #cycles = %lld\n", diff_cycles);
+  
 #if DEBUG
-  // R := 0x5285A2775507B454F7711C4903CFEC324F088DF24DEA948E90C6E99D3755DAC3;
-  mpi_print("  R  = 0x", r, 4);
+  int_to_hex(resh, rf, NWORDS);
+  printf("  R  = %s\n", resh);
+  static const unsigned char rh[] =  // expected result for R
+    "0x5285A2775507B454F7711C4903CFEC324F088DF24DEA948E90C6E99D3755DAC3";
+  if (strcmp(rh, resh) != 0) printf("  result R is wrong!!!\n");
 #endif
 
   printf("=============================================================\n");
 }
 
-void test_ecdh()
+
+void test_ecdh(void)
 {
+  Word sk_a[NWORDS], sk_b[NWORDS];  // secret keys
+  Word pk_a[NWORDS], pk_b[NWORDS];  // public keys
+  Word ss_a[NWORDS], ss_b[NWORDS];  // shared secrets
+  static Word base_pt[NWORDS] = { 9 };
+  int wrong = 0;
+  
+  static const unsigned char RFC7748_A[] =  // RFC7748 test vector
+    "0x2A2CB91DA5FB77B12A99C0EB872F4CDF4566B25172C1163C7DA518730A6D0777";
+  static const unsigned char RFC7748_B[] =  // RFC7748 test vector
+    "0xEBE088FF278B2F1CFDB6182629B13B6FE60E80838B7FE1794B8A4A627E08AB5D";
+  
   printf("=============================================================\n");
   printf("ecdh correctness test:\n");
   printf("=============================================================\n");
   
-  // RFC7748 test vectors
-  static uint32_t RFC7748_A[8] = { 
-    0x0a6d0777, 0x7da51873, 0x72c1163c, 0x4566b251, 
-    0x872f4cdf, 0x2a99c0eb, 0xa5fb77b1, 0x2a2cb91d };
-  static uint32_t RFC7748_B[8] = {
-    0x7e08ab5d, 0x4b8a4a62, 0x8b7fe179, 0xe60e8083, 
-    0x29b13b6f, 0xfdb61826, 0x278b2f1c, 0xebe088ff };
-
-  union tvec { uint32_t t32[8]; uint64_t t64[4]; };
-  union tvec sk_a, sk_b, pk_a, pk_b, ss_a, ss_b;
-  static union tvec base_pt = { 9 };
-  int wrong = 0;
-
   printf("x25519: (alice <---> bob: rfc7748 test vectors)\n");
-
-  memcpy(sk_a.t64, RFC7748_A, 32);
-  memcpy(sk_b.t64, RFC7748_B, 32);
-
+  
+  hex_to_int(sk_a, RFC7748_A, NWORDS);
+  hex_to_int(sk_b, RFC7748_B, NWORDS);
+  
   printf("- private keys:\n");
-  mpi_print("  alice: ", sk_a.t64, 4);
-  mpi_print("  bob  : ", sk_b.t64, 4);
-
-  mon_mul_varbase(pk_a.t64, sk_a.t64, base_pt.t64);
-  mon_mul_varbase(pk_b.t64, sk_b.t64, base_pt.t64);
-
+  mpi_print("  alice: ", sk_a, NWORDS);
+  mpi_print("  bob  : ", sk_b, NWORDS);
+  
+  mon_mul_varbase(pk_a, sk_a, base_pt);
+  mon_mul_varbase(pk_b, sk_b, base_pt);
+  
   printf("- public keys:\n");
-  mpi_print("  alice: ", pk_a.t64, 4);
-  mpi_print("  bob  : ", pk_b.t64, 4);
-
-  mon_mul_varbase(ss_a.t64, sk_a.t64, pk_b.t64);
-  mon_mul_varbase(ss_b.t64, sk_b.t64, pk_a.t64);
-
+  mpi_print("  alice: ", pk_a, NWORDS);
+  mpi_print("  bob  : ", pk_b, NWORDS);
+  
+  mon_mul_varbase(ss_a, sk_a, pk_b);
+  mon_mul_varbase(ss_b, sk_b, pk_a);
+  
   printf("- shared secrets:\n");
-  mpi_print("  alice: ", ss_a.t64, 4);
-  mpi_print("  bob  : ", ss_b.t64, 4);
-
-  wrong = memcmp(ss_a.t64, ss_b.t64, 4);
-
+  mpi_print("  alice: ", ss_a, NWORDS);
+  mpi_print("  bob  : ", ss_b, NWORDS);
+  
+  wrong = memcmp(ss_a, ss_b, WBYTES*NWORDS);
+  
   printf("- results:");
-
+  
   if (wrong == 0) printf("\x1b[32m equal \x1b[0m\n");
   else            printf("\x1b[31m not equal \x1b[0m\n");
-
+  
   printf("-------------------------------------------------------------\n");
-
+  
   printf("x25519: (carol <---> dave: random test vectors)\n");
-
-  rand_bytes((uint8_t *)sk_a.t64, 32);
-  rand_bytes((uint8_t *)sk_b.t64, 32);
-
+  
+  rand_bytes((uint8_t *) sk_a, WBYTES*NWORDS);
+  rand_bytes((uint8_t *) sk_b, WBYTES*NWORDS);
+  
   printf("- private keys:\n");
-  mpi_print("  carol: ", sk_a.t64, 4);
-  mpi_print("  dave : ", sk_b.t64, 4);
-
-  mon_mul_varbase(pk_a.t64, sk_a.t64, base_pt.t64);
-  mon_mul_varbase(pk_b.t64, sk_b.t64, base_pt.t64);
-
+  mpi_print("  carol: ", sk_a, NWORDS);
+  mpi_print("  dave : ", sk_b, NWORDS);
+  
+  mon_mul_varbase(pk_a, sk_a, base_pt);
+  mon_mul_varbase(pk_b, sk_b, base_pt);
+  
   printf("- public keys:\n");
-  mpi_print("  carol: ", pk_a.t64, 4);
-  mpi_print("  dave : ", pk_b.t64, 4);
-
-  mon_mul_varbase(ss_a.t64, sk_a.t64, pk_b.t64);
-  mon_mul_varbase(ss_b.t64, sk_b.t64, pk_a.t64);
-
+  mpi_print("  carol: ", pk_a, NWORDS);
+  mpi_print("  dave : ", pk_b, NWORDS);
+  
+  mon_mul_varbase(ss_a, sk_a, pk_b);
+  mon_mul_varbase(ss_b, sk_b, pk_a);
+  
   printf("- shared secrets:\n");
-  mpi_print("  carol: ", ss_a.t64, 4);
-  mpi_print("  dave : ", ss_b.t64, 4);
-
-  wrong = memcmp(ss_a.t64, ss_b.t64, 4);
-
+  mpi_print("  carol: ", ss_a, NWORDS);
+  mpi_print("  dave : ", ss_b, NWORDS);
+  
+  wrong = memcmp(ss_a, ss_b, WBYTES*NWORDS);
+  
   printf("- results:");
-
+  
   if (wrong == 0) printf("\x1b[32m equal \x1b[0m\n");
   else            printf("\x1b[31m not equal \x1b[0m\n");
 }
+
 
 int main( int argc, char* argv[] ) {
   #if defined( MPISE_ISE ) && defined( MPISE_STATELESS ) && ( MPISE_STATELESS == 0 )
   asm( "csrrwi x0, 0x801, " MPISE_RADIX_IMM );
   #endif
-
+  
+  test_gfp_arith(1000);
+  test_curve_arith(50);
   test_ecdh();
-  test_gfp_arith();
-  test_curve_arith();
-
+  
   return 0;
 }
