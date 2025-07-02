04f59a8e299d1dfa6ece1fb5c66eb3707da653f4
Dan Page <dan@phoo.org>
2025-07-02 15:25:55 +0100

    bump

--- src/sw/intmul/plot.sh
+++ src/sw/intmul/plot.sh
@@ -1,16 +1,39 @@
+#!/bin/bash 
+
+plot() {
+  # plot ISA-based results
+  export TYPE="isa"
+  export VERSION="simple"
+  export OUTPUT="./scan/intmul-${ARCH}_${TYPE}_${VERSION}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf"
+  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="80000" --scale-y-step="10000" 
+  export TYPE="isa"
+  export VERSION="hybrid"
+  export OUTPUT="./scan/intmul-${ARCH}_${TYPE}_${VERSION}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf"
+  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="80000" --scale-y-step="10000" 
+  # plot ISE-based results
+  export TYPE="ise"
+  export VERSION="simple"
+  export OUTPUT="./scan/intmul-${ARCH}_${TYPE}_${VERSION}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf"
+  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="80000" --scale-y-step="10000" 
+  export TYPE="ise"
+  export VERSION="hybrid"
+  export OUTPUT="./scan/intmul-${ARCH}_${TYPE}_${VERSION}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf"
+  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="80000" --scale-y-step="10000" 
+}
+
 rm -rf ./scan/*
 
 export MPISE_DESTRUCTIVE="0"
 export MPISE_STATELESS="0"
 
 export ARCH="rv32"
+export XLEN="32"
 cp ${REPO_HOME}/data/intmul-fpga-250627/generated-logs/${ARCH}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}/*.log ./scan
-PLATFORM="" RISCV="" bash scan.sh plot-fpga
-rm ./scan/intmul-${ARCH}_2x2_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf
+plot
 
 export ARCH="rv64"
+export XLEN="64"
 cp ${REPO_HOME}/data/intmul-fpga-250627/generated-logs/${ARCH}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}/*.log ./scan
-PLATFORM="" RISCV="" bash scan.sh plot-fpga
-rm ./scan/intmul-${ARCH}_2x2_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf
+plot
 
-
+cp ./scan/*.pdf ${1}/image
--- src/sw/intmul/scan.py
+++ src/sw/intmul/scan.py
@@ -43,6 +43,18 @@ def plot( data ) :
 
   fig.set_figheight( argv.output_ht )
   fig.set_figwidth ( argv.output_wd )
+
+  if ( ( argv.scale_x_max != None ) and ( argv.scale_x_step != None ) ) :
+    xticks = np.arange( 0, argv.scale_x_max, argv.scale_x_step )
+    xlabels = [ '${0:d}$'.format( x ) for x in xticks ]
+    ax.set_xlim( 0, argv.scale_x_max )
+    ax.set_xticks( xticks, labels = xlabels )
+
+  if ( ( argv.scale_y_max != None ) and ( argv.scale_y_step != None ) ) :
+    yticks = np.arange( 0, argv.scale_y_max, argv.scale_y_step )
+    ylabels = [ '${0:d}$'.format( y ) for y in yticks ]
+    ax.set_ylim( 0, argv.scale_y_max )
+    ax.set_yticks( yticks, labels = ylabels )
   
   for ( i, radix ) in enumerate( sorted( data.keys() ) ) :
     xs     = [ x            for ( _, x, _, _, _ ) in data[ radix ] ]
@@ -54,9 +66,11 @@ def plot( data ) :
     es     = [ es_min, es_max ]
   
     c = colors[ i ] ; m = '*' if ( radix == 64 ) else 'x'
-  
-    ax.plot( xs, ys, c = c, linestyle = 'dotted', marker = m, label = 'Radix $2^{{{0:d}}}$'.format( radix ) )
-    plt.errorbar( xs, ys, yerr = es, ecolor = c, elinewidth = 1, capsize = 2, fmt = 'none' )
+
+    ax.plot( xs, ys, c = c, linestyle = 'dotted', marker = m, label = 'Radix-$2^{{{0:d}}}$'.format( radix ) )
+
+    if ( argv.error_bars ) :
+      plt.errorbar( xs, ys, yerr = es, ecolor = c, elinewidth = 1, capsize = 2, fmt = 'none' )
   
   ax.set( xlabel = 'Operand length (bits)', ylabel = 'Cycles', title = r'$\mbox{{type}} = \mbox{{{0:s}}}$, $\mbox{{\tt xlen}} = {1:s}$, $\mbox{{\tt version}} = {2:s}$'.format( argv.type.upper(), argv.xlen, argv.version ) )
   ax.legend( loc = 'upper left' ) ; ax.grid( True )
@@ -66,14 +80,21 @@ def plot( data ) :
 if ( __name__ == '__main__' ) :
   parser = argparse.ArgumentParser( add_help = False )
 
-  parser.add_argument( '--type',   action = 'store', choices = [ 'isa', 'ise' ] )
-  parser.add_argument( '--xlen',   action = 'store', choices = [  '32',  '64' ] )
-  parser.add_argument( '--version', action = 'store', choices = [ 'simple', 'hybrid' ] )
+  parser.add_argument( '--type',         action = 'store', choices = [ 'isa', 'ise' ] )
+  parser.add_argument( '--xlen',         action = 'store', choices = [  '32',  '64' ] )
+  parser.add_argument( '--version',      action = 'store', choices = [ 'simple', 'hybrid' ] )
 
-  parser.add_argument( '--output-ht', action = 'store', type = int, default =  6 )
-  parser.add_argument( '--output-wd', action = 'store', type = int, default = 12 )
+  parser.add_argument( '--error-bars',   action = 'store_true' )
 
-  parser.add_argument( '--output',    action = 'store', type = str )
+  parser.add_argument( '--scale-x-max',  action = 'store', type = int, default = None )
+  parser.add_argument( '--scale-x-step', action = 'store', type = int, default = None )
+  parser.add_argument( '--scale-y-max',  action = 'store', type = int, default = None )
+  parser.add_argument( '--scale-y-step', action = 'store', type = int, default = None )
+
+  parser.add_argument( '--output-ht',    action = 'store', type = int, default =   10 )
+  parser.add_argument( '--output-wd',    action = 'store', type = int, default =   12 )
+
+  parser.add_argument( '--output',       action = 'store', type = str, default = None )
 
   argv = parser.parse_args()
 
