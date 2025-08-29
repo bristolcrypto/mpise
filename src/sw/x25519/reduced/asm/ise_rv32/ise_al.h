0e273689fbfeb175a944acfb176c11efbab80adc
Johann Großschädl <johann.groszschaedl@uni.lu>
2025-08-29 12:36:16 +0200

    Initial version of reduced-radix ISE Assembly functions

---
+++ src/sw/x25519/reduced/asm/ise_rv32/ise_al.h
@@ -0,0 +1,60 @@
+// Copyright (C) 2021 SCARV project <info@scarv.org>
+//
+// Use of this source code is restricted per the MIT license, a copy of which 
+// can be found at https://opensource.org/licenses/MIT (or should be included 
+// as LICENSE.txt within the associated archive or repository).
+
+// ============================================================================
+
+#ifndef _ISE_AL_H
+#define _ISE_AL_H
+
+#include "ise.h"
+
+.macro madd51lu Z, A, B, S
+#if   defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 0 )
+// !destructive
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+// !destructive && !stateless
+  macclu \Z, \A, \B, \S
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+// !destructive &&  stateless
+  macclu \Z, \A, \B, \S, MPISE_RADIX_IMM
+#endif
+#elif defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 1 )
+//  destructive
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+//  destructive && !stateless
+  macclu \Z, \A, \B
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+//  destructive &&  stateless
+  macclu \Z, \A, \B,     MPISE_RADIX_IMM
+#endif
+#endif
+.endm
+
+.macro madd51hu Z, A, B, S
+#if   defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 0 )
+// !destructive
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+// !destructive && !stateless
+  macchu \Z, \A, \B, \S
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+// !destructive &&  stateless
+  macchu \Z, \A, \B, \S, MPISE_RADIX_IMM
+#endif
+#elif defined( MPISE_DESTRUCTIVE ) && ( MPISE_DESTRUCTIVE == 1 )
+//  destructive
+#if   defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 0 )
+//  destructive && !stateless
+  macchu \Z, \A, \B
+#elif defined( MPISE_STATELESS   ) && ( MPISE_STATELESS   == 1 )
+//  destructive &&  stateless
+  macchu \Z, \A, \B,     MPISE_RADIX_IMM
+#endif
+#endif
+.endm
+
+// ============================================================================                              
+
+#endif 
