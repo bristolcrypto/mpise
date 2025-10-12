ca77cf8d1fea8337c9227cc5e3ecba8ff9a2fdaf
Dan Page <dan@phoo.org>
2025-10-12 19:16:18 +0100

    bump

--- src/sw/intmul/plot.sh
+++ src/sw/intmul/plot.sh
@@ -1,27 +1,28 @@
 #!/bin/bash 
 
 #export ARCHIVE="${REPO_HOME}/data/intmul-fpga-250627"
- export ARCHIVE="${REPO_HOME}/data/intmul-verilator-250930"
+#export ARCHIVE="${REPO_HOME}/data/intmul-verilator-250930"
+ export ARCHIVE="${REPO_HOME}/data/intmul-verilator-251011"
 
 plot() {
   # plot ISA-based results
   export TYPE="isa"
   export VERSION="simple"
   export OUTPUT="./scan/intmul-${ARCH}_${TYPE}_${VERSION}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf"
-  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="80000" --scale-y-step="10000" --show-crossover
+  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --output-ht="10" --output-wd="16" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="50000" --scale-y-step="10000" --show-crossover
   export TYPE="isa"
   export VERSION="hybrid"
   export OUTPUT="./scan/intmul-${ARCH}_${TYPE}_${VERSION}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf"
-  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="80000" --scale-y-step="10000" --show-crossover
+  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --output-ht="10" --output-wd="16" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="50000" --scale-y-step="10000" --show-crossover
   # plot ISE-based results
   export TYPE="ise"
   export VERSION="simple"
   export OUTPUT="./scan/intmul-${ARCH}_${TYPE}_${VERSION}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf"
-  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="80000" --scale-y-step="10000" --show-crossover
+  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --output-ht="10" --output-wd="16" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="50000" --scale-y-step="10000" --show-crossover
   export TYPE="ise"
   export VERSION="hybrid"
   export OUTPUT="./scan/intmul-${ARCH}_${TYPE}_${VERSION}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS}.pdf"
-  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="80000" --scale-y-step="10000" --show-crossover
+  cat ./scan/intmul-${ARCH}_${TYPE}_*_${VERSION}.log | grep '^!' | cut -c 3- | python3 ./scan.py --type="${TYPE}" --xlen="${XLEN}" --version="${VERSION}" --output="${OUTPUT}" --output-ht="10" --output-wd="16" --scale-x-max="4500" --scale-x-step="256" --scale-y-max="50000" --scale-y-step="10000" --show-crossover
 }
 
 rm -rf ./scan/*
@@ -40,7 +41,7 @@ cp ${ARCHIVE}/generated-logs/${ARCH}_mpise_${MPISE_DESTRUCTIVE}${MPISE_STATELESS
 plot
 
 if [ ! -z "${1}" ] ; then
-  cp ./scan/*.pdf ${1}/image
+  cp ./scan/*.pdf ${1}
 else
   echo "!! not copying the results"
 fi
